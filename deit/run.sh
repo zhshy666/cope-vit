bdf19b0f9241cea985999d39841e868fea4252df
zhshy666 <18302010080@fudan.edu.cn>
2025-05-19 19:33:53 +0800

    update environments

--- deit/run.sh
+++ deit/run.sh
@@ -2,7 +2,7 @@ conda activate cv
 
 python3 -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model cope_2d_v2_deit_small_patch16_LS_q --data-path ../data/tiny-imagenet-200/ --output_dir ../save/[new]2d-cope+ape+q/ --data-set TINY --batch-size 128 --epochs 400 --input-size 64 --wandb-project Rope-2D-tiny --wandb-name "[new]2d cope+ape+q"
 
-python3 -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model cope_2d_v2_deit_small_patch16_LS --data-path ../data/tiny-imagenet-200/ --output_dir ../save/[new]2d-cope+ape+k/ --data-set TINY --batch-size 128 --epochs 400 --input-size 64 --wandb-project Rope-2D-tiny --wandb-name "[new]2d cope+ape+k"
+python3 -m torch.distributed.launch --nproc_per_node=1 --use_env main.py --model cope_2d_v2_deit_small_patch16_LS --data-path ../data/tiny-imagenet-200/ --output_dir ../save/[new]2d-cope+ape+k/ --data-set TINY --batch-size 128 --epochs 400 --input-size 64 --wandb-project Rope-2D-tiny --wandb-name "[new]2d cope+ape+k"
 
 python3 -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model cope_2d_v2_deit_small_patch16_LS_v --data-path ../data/tiny-imagenet-200/ --output_dir ../save/[new]2d-cope+ape+v/ --data-set TINY --batch-size 128 --epochs 400 --input-size 64 --wandb-project Rope-2D-tiny --wandb-name "[new]2d cope+ape+v"
 
